c184ce40db57d52f25b5305f26bea36904075114
SnehaAjagekar <snehaajagekar@gmail.com>
2025-01-09 23:31:40 +0530

    Some changes made using api

--- SocialMedia/src/components/PostList.jsx
+++ SocialMedia/src/components/PostList.jsx
@@ -1,23 +1,25 @@
 import Post from "./Post";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { PostList as PostListContext } from "../store/post-list-store"; // Assuming this is a context
 import WelcomeMessage from "./WelcomeMessage";
 const PostList = () => {
   const { postList, addInitialPost } = useContext(PostListContext);
-  const handleGetPostClick = () =>{
+  useEffect(() =>{
     fetch('https://dummyjson.com/posts')
     .then(res => res.json())
     .then((data) => {
       addInitialPost(data.posts)
     });
-  }
+  }, []);
+    
+  
   return (
     <>
       {postList && postList.length > 0 ? (
         postList.map((post) => <Post key={post.id} post={post} />)
       ) 
       : (
-        <WelcomeMessage onGetPostsClick={handleGetPostClick}/>// Fallback for empty or undefined postList
+        <WelcomeMessage/>// Fallback for empty or undefined postList
       )
       }
     </>
--- SocialMedia/src/components/WelcomeMessage.jsx
+++ SocialMedia/src/components/WelcomeMessage.jsx
@@ -2,9 +2,7 @@ const WelcomeMessage = ({onGetPostsClick}) => {
     return (
     <center className="welcome-message">
         <h3 >There are no posts</h3>
-        <button type="button" onClick={onGetPostsClick} className="btn btn-primary">
-            Get posts from server
-        </button>
+        
     </center>
     )
 }
